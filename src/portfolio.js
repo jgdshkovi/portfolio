0f2b5f791d3b4eb63b7115db924d37fdc4dfc46a
jagakovi <jgdshkovi@gmail.com>
2024-06-13 19:21:47 -0400

    added proj, box shadows

--- src/portfolio.js
+++ src/portfolio.js
@@ -12,7 +12,7 @@ const about = {
   role2: 'Ex-Software Engineer@FIS ',
   description:
     "Jagadeesh Kovi is a passionate Data Science professional and Software Engineer with a proven track record of leveraging technology to drive innovation in the financial services sector. Graduating with a Master's in Data Science from Indiana University Bloomington, Jagadeesh possesses a rich academic foundation complemented by hands-on experience at industry-leading companies like FIS Global. With expertise spanning Python, Java, SQL, and cutting-edge technologies like Azure, he has played pivotal roles in modernizing core banking platforms, enhancing application efficiency, and translating intricate business requirements into tangible solutions. Adept at bridging the gap between technical intricacies and business objectives, Jagadeesh's commitment to excellence, combined with his analytical prowess, makes him a valuable asset in any forward-thinking organization.",
-  resume: 'https://drive.google.com/file/d/1dIPGtgpAFZ5goYG5h6J9GT123S9I74la/view',
+  resume: 'https://drive.google.com/uc?export=download&id=1eZG3fTKeIKL_CyaPULa5h3xKX35xSt6t',
   social: {
     linkedin: 'https://linkedin.com/in/jgdshkovi',
     github: 'https://github.com/jgdshkovi',
@@ -23,15 +23,64 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
+    name: '(Currently developing) ChatBot using LangChain',
+    description:
+      `• Document Helper Bot, an LLM application designed to simplify information access (RAG) from docs through a Q&A interface. 
+      Initially using OpenAI API during development but plan to integrate open-source models later.
+
+      • Leverages LangChain’s power, goes beyond traditional solutions, and is customizable for specific data sets.`,
+    stack: ['LangChain,', 'Pinecone VectorDB,', 'OpenAI LLM'],
+    sourceCode: '',
+    livePreview: '',
+  },
+  {
+    name: 'Weather Data GCP Pipeline',
+    description:
+      `• Designed and implemented an automated ETL pipeline on Google Cloud Platform for weather data management.
+      Utilized Python, Pandas, and OpenWeather API for data extraction, transformation, and storage in BigQuery.
+      
+      • Deployed and scheduled daily data processing using GCP services like Cloud Functions and Cloud Scheduler.
+      Created interactive data visualizations and dashboards using Looker Studio for weather data analysis.`,
+    stack: ['ETL,', 'GCP Cloud Storage/Functions/Scheduler,', 'BigQuery,', 'Looker Studio'],
+    sourceCode: '',
+    livePreview: '',
+  },
+  {
+    name: 'Object Recognition on Historic IULMIA Ad Videos',
+    description:
+      `• Enhanced IU Libraries Moving Image Archive accessibility by leveraging DINO+SAM architecture and YOLOv8
+      models for object detection and text recognition in historic television ads.
+      
+      • Improved historical footage object detection by 20% through unsupervised learning on labeled datasets from video
+      frames, with room for enhancement via OCR and optimized open-set object detection class selection.`,
+    stack: ['Grounding DINO + SAM,', 'Open-set object detection,', 'YOLO'],
+    sourceCode: '',
+    livePreview: '',
+  },
+  {
+    name: 'OMR Sheet Processing and Answer Extraction',
+    description:
+      `• Developed an automated system for processing scanned answer sheets and extracting student responses. Achieved
+      over 99% accuracy on validation dataset, processing all the forms in under 3 seconds.
+      
+      • Experimented & implemented Image preprocessing, contour detection, steganography, and custom
+      barcode encoding/decoding. Designed the system to be efficient, robust & handle specific data constraints.`,
+    stack: ['Python,', 'PIL,', 'OpenCV,', 'Morphological Ops'],
+    sourceCode: '',
+    livePreview: '',
+  },
+  {
     name: 'Home Credit Default Risk Analysis',
     description:
       `• Designed and optimized an advanced Multi-Layer Perceptron (MLP) using back-propagation and gradient descent, achieving
       a crucial 68.26% test accuracy, contributing to Home Credit’s lending strategy refinement.
+      
       • Employed feature selection techniques like grid search and cross-validation for hyperparameter tuning, enhancing the Deep
       Learning model to achieve a test F1 score (0.7311) and a public score (0.7537).
+      
       • Executed a varied ensemble strategy with Random Forest, CatBoost, and XGBoost via stacking. Integrated a deep learning
       algorithm, boosting predictive accuracy by 10%.`,
-    stack: ['Python', 'Sklearn,', 'Feature Selection', 'Hyperparameter tuning', 'TensorFlow'],
+    stack: ['Python,', 'Sklearn,', 'Feature Selection,', 'Hyperparameter tuning,', 'TensorFlow'],
     sourceCode: '',
     livePreview: '',
   },
@@ -39,15 +88,17 @@ const projects = [
     name: 'Automated Web Scraping & Google Translate',
     description:
       `• Employed requests and BeautifulSoup(bs4) Python libraries to automate data extraction from provided URLs.
+      
       • Implemented a robust website scraping solution with customizable parameters, including element types and
       scraping depth(up to 3), to efficiently organize and manage the extracted data.
+      
       • To showcase, automated the translation of a large sample website to Hindi using Google Translate API.`,
       // [
       //   "• Employed requests and BeautifulSoup(bs4) Python libraries to automate data extraction from provided URLs."
       //   `• Implemented a robust website scraping solution with customizable parameters, including element types and
       //   scraping depth(up to 3), to efficiently organize and manage the extracted data.`,
       //   "• To showcase, automated the translation of a large sample website to Hindi using Google Translate API."],
-    stack: ['Python', 'BeautifulSoup', 'Requests'],
+    stack: ['Python,', 'BeautifulSoup,', 'Requests'],
     sourceCode: 'https://github.com/jgdshkovi/webscrape_and_translate',
     livePreview: 'https://jgdshkovi-abc.000webhostapp.com/',
   },
@@ -56,11 +107,13 @@ const projects = [
     description:
       `• Successfully fine-tuned the BERT model (transfer learning) by leveraging the Jigsaw Toxic comment dataset from
       Kaggle with ∼159k observations, enhancing its performance for accurate toxic comment classification.
+      
       • Implemented an integrated system that extracts tweets from the user’s timeline using the Twitter API and
       seamlessly passes them to the trained NLP model for analysis.
+      
       • The trained model achieved an validation accuracy of 95.8%. Classify the tweets, and take appropriate action
       based on the returned toxicity score, contributing to a safer online environment.`,
-    stack: ['Python', 'Flask', 'Tweepy', 'NLP', 'Libraries'],
+    stack: ['Python,', 'Flask,', 'Tweepy,', 'NLP,', 'Libraries'],
     sourceCode: 'https://github.com/jgdshkovi/final_yr_proj',
     livePreview: '',
   },
